c5a7ca66b83efc0c7d61210cd4466a313aa08f51
rony-obando <ronyalvertoobando19092003@gmail.com>
2021-08-11 17:30:46 -0600

    Agregar archivos de proyecto.

---
+++ si/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace si
+{
+    public class Class1
+    {
+
+        private int id;
+        private string dni;
+        private string nombres;
+        private string apellidos;
+        private decimal salario;
+        public Class1()
+        {
+
+        }
+        public Class1(int id, string dni,string nombres,string apellidos,decimal salario)
+        {
+            this.Id = id;
+            this.DNI = dni;
+            this.Nombres = nombres;
+            this.Apellidos = apellidos;
+            this.Salario = salario;
+        }
+        public int Id
+        {
+            get => this.id;
+            set => this.id = value;
+        }
+        public string DNI
+        {
+            get => this.dni;
+            set => this.dni = value;
+        }
+        public string Nombres
+        {
+            get => this.nombres;
+            set => this.nombres = value;
+        }
+        public string Apellidos
+        {
+            get => this.apellidos;
+            set => this.apellidos = value;
+        }
+        public decimal Salario
+        {
+            get => this.salario;
+            set => this.salario = value;
+        }
+    }
+    
+
+}
---
+++ si/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace si
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            Class1 emp = new Class1()
+            {
+                Id = 1,
+                DNI="001-190903-1044L",
+                Nombres="Rony Alberto",
+                Apellidos="Obando Rodriguez",
+                Salario=7500,
+            };
+            MessageBox.Show($@" Id:{emp.Id}{Environment.NewLine}
+                            DNI:{emp.DNI}{Environment.NewLine}
+                            Nombre:{emp.Nombres}{Environment.NewLine}
+                            Apellidos:{emp.Apellidos}{Environment.NewLine}
+                            Salario:{emp.Salario}{Environment.NewLine}");
+        }
+
+      
+    }
+}
