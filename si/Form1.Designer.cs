b3e875188f66144f9f24f17733ba32caf467866e
rony-obando <ronyalvertoobando19092003@gmail.com>
2021-08-15 13:36:56 -0600

    Cambio #2(BoxTxt)

--- si/Form1.Designer.cs
+++ si/Form1.Designer.cs
@@ -29,34 +29,126 @@ namespace si
         /// </summary>
         private void InitializeComponent()
         {
+            this.Dni = new System.Windows.Forms.Label();
+            this.Nombres = new System.Windows.Forms.Label();
+            this.Apellidos = new System.Windows.Forms.Label();
+            this.Salario = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // Dni
+            // 
+            this.Dni.AutoSize = true;
+            this.Dni.Location = new System.Drawing.Point(56, 68);
+            this.Dni.Name = "Dni";
+            this.Dni.Size = new System.Drawing.Size(26, 13);
+            this.Dni.TabIndex = 1;
+            this.Dni.Text = "DNI";
+            // 
+            // Nombres
+            // 
+            this.Nombres.AutoSize = true;
+            this.Nombres.Location = new System.Drawing.Point(59, 122);
+            this.Nombres.Name = "Nombres";
+            this.Nombres.Size = new System.Drawing.Size(49, 13);
+            this.Nombres.TabIndex = 2;
+            this.Nombres.Text = "Nombres";
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.AutoSize = true;
+            this.Apellidos.Location = new System.Drawing.Point(59, 165);
+            this.Apellidos.Name = "Apellidos";
+            this.Apellidos.Size = new System.Drawing.Size(49, 13);
+            this.Apellidos.TabIndex = 3;
+            this.Apellidos.Text = "Apellidos";
+            // 
+            // Salario
+            // 
+            this.Salario.AutoSize = true;
+            this.Salario.Location = new System.Drawing.Point(59, 212);
+            this.Salario.Name = "Salario";
+            this.Salario.Size = new System.Drawing.Size(39, 13);
+            this.Salario.TabIndex = 4;
+            this.Salario.Text = "Salario";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(229, 68);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 5;
+            this.txtDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDni_KeyPress);
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(229, 114);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(100, 20);
+            this.txtNombres.TabIndex = 6;
+            this.txtNombres.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombres_KeyPress);
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(229, 165);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidos.TabIndex = 7;
+            this.txtApellidos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellidos_KeyPress);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(229, 212);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 8;
+            this.txtSalario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSalario_KeyPress);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 68);
+            this.button1.Location = new System.Drawing.Point(471, 135);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 290);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.Salario);
+            this.Controls.Add(this.Apellidos);
+            this.Controls.Add(this.Nombres);
+            this.Controls.Add(this.Dni);
             this.Name = "Form1";
             this.Text = "Form1";
-            //this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label Dni;
+        private System.Windows.Forms.Label Nombres;
+        private System.Windows.Forms.Label Apellidos;
+        private System.Windows.Forms.Label Salario;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtSalario;
         private System.Windows.Forms.Button button1;
     }
 }
--- si/Form1.cs
+++ si/Form1.cs
@@ -20,21 +20,90 @@ namespace si
         private void button1_Click(object sender, EventArgs e)
 
         {
+            
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string nombres, apellidos, dni;
+            decimal salario;
+            dni = txtDni.Text;
+            nombres = txtNombres.Text;
+            if (txtNombres.Text.Length>20)
+            {
+                MessageBox.Show($@"Error el nombre:{txtNombres.Text} Tiene muchas letras","Mesaje de error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            apellidos = txtApellidos.Text;
+            if (txtApellidos.Text.Length > 20)
+            {
+                MessageBox.Show($@"Error el nombre:{txtApellidos.Text} Tiene muchas letras", "Mesaje de error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!decimal.TryParse(txtSalario.Text, out salario))
+            {
+                MessageBox.Show($@"Error el salario:{txtSalario.Text} no se pudo convertir", "Mensaje de error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             Class1 emp = new Class1()
             {
                 Id = 1,
-                DNI="001-190903-1044L",
-                Nombres="Rony Alberto",
-                Apellidos="Obando Rodriguez",
-                Salario=7500,
+                DNI = dni,
+                Nombres = nombres,
+                Apellidos = apellidos,
+                Salario = salario,
             };
-            MessageBox.Show($@" Id:{emp.Id}{Environment.NewLine}
+            MessageBox.Show($@"
                             DNI:{emp.DNI}{Environment.NewLine}
                             Nombre:{emp.Nombres}{Environment.NewLine}
                             Apellidos:{emp.Apellidos}{Environment.NewLine}
                             Salario:{emp.Salario}{Environment.NewLine}");
         }
+        private void CleanTexbox()
+        {
+            txtDni.Text = string.Empty;
+            txtNombres.Text = string.Empty;
+            txtApellidos.Text = string.Empty;
+            txtSalario.Text = string.Empty;
 
-      
+        }
+
+        private void txtDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar>=32&& e.KeyChar<47)|| (e.KeyChar >= 58 && e.KeyChar < 255))
+            {
+                MessageBox.Show("Solo números","Alerta",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+        }
+
+        private void txtSalario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar < 43)|| (e.KeyChar >= 58 && e.KeyChar < 255) || (e.KeyChar >= 45 && e.KeyChar < 47))
+            {
+                MessageBox.Show("Solo números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+        }
+
+        private void txtNombres_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar < 57)
+            {
+                MessageBox.Show("Solo Letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+
+        }
+
+        private void txtApellidos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar < 57)
+            {
+                MessageBox.Show("Solo Letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+            }
+
+        }
     }
 }
